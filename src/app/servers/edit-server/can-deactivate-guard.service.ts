e8dff4bcc2354ec79981e5f8ba984cae6104ce98
dksc104868 <thomas.balaban@dcsg.com>
2019-04-01 16:10:22 -0400

    lesson finished

---
+++ src/app/servers/edit-server/can-deactivate-guard.service.ts
@@ -0,0 +1,18 @@
+// A interface is a contract that can be used by another class that forces it to use some logic
+import { Observable } from 'rxjs/Observable';
+import { CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+export interface CanComponentDeactivate {
+    canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean;
+}
+
+export class CanDeactivateGuard implements CanDeactivate<CanComponentDeactivate> {
+    canDeactivate(
+        component: CanComponentDeactivate,
+        currentRoute: ActivatedRouteSnapshot,
+        currentState: RouterStateSnapshot,
+        nextState: RouterStateSnapshot
+    ): Observable<boolean> | Promise<boolean> | boolean {
+        return component.canDeactivate();
+    }
+}
